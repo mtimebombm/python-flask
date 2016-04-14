e70a006f0ba180a614b83ee1c481e31efd775138
zhaimingquan <zhaimingquan@gmail.com>
2016-04-14 09:13:23 -0700

    add echars

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
 	user = { 'nickname': 'Miguel' } # fake user
 	posts = [ # fake array of posts
@@ -26,3 +25,11 @@ def login():
 		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
 		return redirect('/index')
 	return render_template('login.html', title = 'Sign In', form=form, providers = app.config['OPENID_PROVIDERS'])
+
+@app.route('/echar_test')
+def echar_test():
+	return render_template("echar_test.html")
+
+@app.route('/echarts.min.js')
+def echar_js():
+	return render_template("echarts.min.js")
