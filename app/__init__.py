60496452efb277f013338be310d08934b441bed1
zhaimingquan <zhaimingquan@gmail.com>
2016-05-01 05:51:14 -0700

    添加链接数据库功能，后续增加直接从数据库读数据的功能

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,9 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views,models
+from pandas import Series,DataFrame
+import pandas as pd
+import numpy as np
+import MySQLdb
+import pandas.io.sql as sql
+
